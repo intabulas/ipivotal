455b4c8bceb3d68be7d0d5e62daf9c806ca8875d
Mark Lussier <mlussier@gmail.com>
2009-08-30 15:50:03 -0700

    basic support for tasks. Not in the UI yet

--- Classes/PivotalTask.m
+++ Classes/PivotalTask.m
@@ -1,7 +1,33 @@
 
 #import "PivotalTask.h"
+#import "ASIHTTPRequest.h"
+#import "PivotalStory.h"
+#import "PivotalProject.h"
+#import "PivotalNoteParserDelegate.h"
+
+@interface PivotalTask ()
+@end
 
 
 @implementation PivotalTask
+@synthesize taskId, description, position, complete, createdAt;
+
+-(id)initWithProject:(PivotalProject *)theProject andStory:(PivotalStory *)theStory {
+    [super init];
+    project = theProject;
+    story = theStory;
+    return self;
+}
+
+
+
+-(void)dealloc {
+    [description release]; description = nil;
+    [createdAt release]; createdAt = nil;
+    [super dealloc];
+}
+
+
+
 
 @end
