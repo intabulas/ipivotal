4f6742ae71846447a574076d09662da5066805e1
Mark Lussier <mlussier@gmail.com>
2010-07-09 10:26:01 -0700

    cleaned up the story comments header

--- Classes/CommentHeaderView.m
+++ Classes/CommentHeaderView.m
@@ -45,12 +45,12 @@
 //        [self setBackgroundColor: [UIColor colorWithRed:76.0/255.0 green:76.0/255.0 blue:76.0/255.0 alpha:1.0]];
         [self setBackgroundColor:[UIColor grayColor]];
         
-        commentImage = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 26.0f, 22.0f)];
+        commentImage = [[UIImageView alloc] initWithFrame:CGRectMake(3.0f, 3.0f, 16.0f, 16.0f)];
         commentImage.backgroundColor = [UIColor clearColor];
         commentImage.contentMode = UIViewContentModeCenter;
-        commentImage.image = [UIImage imageNamed:kIconCommentSmall];
+        commentImage.image = [UIImage imageNamed:kIconComment];
         
-        commentTitle = [[UILabel alloc] initWithFrame:CGRectMake(30.0f, -17.0f, (self.frame.size.width - 30.0f) , self.frame.size.height)];
+        commentTitle = [[UILabel alloc] initWithFrame:CGRectMake(25.0f, -18.0f, (self.frame.size.width - 30.0f) , self.frame.size.height)];
 		commentTitle.autoresizingMask = UIViewAutoresizingNone;
 		commentTitle.backgroundColor = [UIColor clearColor];
 		commentTitle.highlightedTextColor = [UIColor whiteColor];
